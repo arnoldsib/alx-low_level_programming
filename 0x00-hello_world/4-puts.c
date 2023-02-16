fd3c2f419d026992129d48bd380d5780622614a3
arnoldsib <arnoldsibita@gmail.com>
2023-02-16 10:03:45 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"programming is like buildingmultilingual puzzle");
+    return (0);
+}
