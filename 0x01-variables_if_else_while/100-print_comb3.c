f17235b26d581fb17d2c1415c48a5bb86dd4b0c2
arnoldsib <arnoldsibita@gmail.com>
2023-02-17 02:45:33 -0800

    Advanced tasks

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,31 +7,31 @@
  */
 int main(void)
 {
-int i, e;
+	int i, e;
 
-i = 48;
-e = 48;
+	i = 48;
+	e = 48;
 
-while (e < 58)
-{
-i = 48;
-while (i < 58)
-{
-if (e != i && e < i)
-{
-putchar(e);
-putchar(i);
-if (i == 57 && e == 56)
-{
-break;
-}				
-putchar(',');
-putchar(' ');
-}
-i++;
-}
-e++;
-}	
-putchar('\n');
-return (0);
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
 }
