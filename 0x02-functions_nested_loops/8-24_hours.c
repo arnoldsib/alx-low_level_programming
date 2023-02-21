16d787cf3c5cd20803aa6a341c123a6d6ca081a3
arnoldsib <arnoldsibita@gmail.com>
2023-02-21 05:05:56 -0800

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,35 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints the minutes of a day
- *
- * Return: no return
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ * Return: ...
  */
+
 void jack_bauer(void)
+
 {
+
 	int a, b, c, d;
 
-	for (a = 48; a <= 50; a++)
+	for (a = 0; a <= 2; a++)
 	{
-		for (b = 48; b <= 57; b++)
-		{
-			for (c = 48; c <= 53; b++)
-			{
-				for (d = 48; d <= 57; d++)
-				{
-					if (a >= 50 && b >= 52)
-						break;
-					_putchar(a);
-					_putchar(b);
-					_putchar(58);
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-				}
-			}
-		}
+	for (b = 0; b <= 9; b++)
+	{
+	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+	{
+	for (c = 0; c <= 5; c++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(58);
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	}
 	}
 }
