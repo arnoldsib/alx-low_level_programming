798f6d384bca64e2478c6877a57e2f8d1a63c88f
arnoldsib <arnoldsibita@gmail.com>
2023-02-23 03:02:06 -0800

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57 ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
