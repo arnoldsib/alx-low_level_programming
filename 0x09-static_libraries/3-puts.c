4eea5149f2aa59f63eaa2cf2868d0d846ee4352c
arnoldsib <arnoldsibita@gmail.com>
2023-03-09 21:50:43 -0800

    pushing my 0x09-static_libraries directory

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+}
+
