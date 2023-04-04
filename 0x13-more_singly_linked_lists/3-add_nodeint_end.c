08a699f3665277e8417ea4b2be3d4f31c4c211d9
arnoldsib <arnoldsibita@gmail.com>
2023-04-04 13:35:24 -0700

    adding the 3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Function adds node at the end.
+ * @head: pointer to struct
+ * @n: number
+ *
+ * Return: the address ot the new element or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t)), *last = *head;
+
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (!(*head))
+	{
+		*head = new;
+	}
+	else
+	{
+		while (last->next)
+		{
+			last = last->next;
+		}
+		last->next = new;
+	}
+	return (*head);
+}
+
