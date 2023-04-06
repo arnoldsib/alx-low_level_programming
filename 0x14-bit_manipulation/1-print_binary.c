33fb7b56d64192f74457fa33e02ab472fd3a28fc
arnoldsib <arnoldsibita@gmail.com>
2023-04-06 05:00:29 -0700

    pushing the 1-print_binary.c file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - Function that prints the binary 
+ * represation of a number.
+ * @n: the number we are going to print
+ * Return: no return value.
+ **/
+void print_binary(unsigned long int n)
+{
+	if (n > 0)
+	{
+		if (n > 1) 
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+		_putchar('0');
+}
