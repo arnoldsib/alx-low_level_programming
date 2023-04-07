725b6234005600f6e507bf60deda46cb1146cb31
arnoldsib <arnoldsibita@gmail.com>
2023-04-07 05:24:48 -0700

    adding the 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int fp, len;
 	ssize_t wdone;
 
-	fp = open(filename, O_CREAT | O_WRONLY| O_TRUNC, 0600);
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if ((fp == -1) | (filename == NULL))
 		return (-1);
 	if (text_content)
